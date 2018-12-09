71ce8ba90e5ad5684bcdae9c6b177aa1a186884a
codingdiva80 <32786907+codingdiva80@users.noreply.github.com>
2018-12-08 21:26:15 -0800

    Jobposting (#11)
    * Added api config
    
    * Updated button
    
    * Updated button
    
    * Updated...

--- src/components/job-search/job-search-foundation.vue
+++ src/components/job-search/job-search-foundation.vue
@@ -10,17 +10,31 @@
         <div class='col'>
           <div class='search-icon'></div>
           <div class='search-input-container'>
-            <input type='text' class='search-input' placeholder='Job keyword' />
+            <input type='text' class='search-input' placeholder='Job keyword' v-model="searchCriteria" />
+            <datalist id="datalist-locations" />
           </div>
         </div>
         <div class='col'>
           <div class='location-icon'></div>
           <div class='search-location-container'>
-            <input type='text' class='search-input' placeholder='Location' />
+            <input type='text' 
+              class='search-input' 
+              placeholder='Location' 
+              @keyup="getLocations"  
+              id="location-search" 
+              v-model="searchLocation"  
+            />
+            <datalist id="datalist-locations" />
           </div>
-        </div>
+        </div> 
+        <div class='col'>
+          <a class='search-button' @mousedown="clearSearch" @mouseup="doSearch">Search</a>
+        </div>         
       </div>
       
+      <div>
+        <job-search-results v-if="searching" />
+      </div>
     </div>
     <MainFooter />
   </div>
@@ -29,13 +43,62 @@
 <script>
   import MainHeader from '@/components/main/main-header';
   import MainFooter from '@/components/main/main-footer';
+  import cities from '@/assets/data/usaCities';
+  import JobSearchResults from './job-search-results';
+  import { SET_JOB_SEARCH } from '@/store/mutation-types';
 
   export default {
     name: 'JobSearch', 
     components: {
       MainHeader,
       MainFooter,
-    }
+      JobSearchResults,
+    },
+    data() {
+      return {
+        minLength: false,
+        searchCriteria: '',
+        searchLocation: '',
+      }
+    },
+    methods: {
+      getLocations($event){
+        const locationSearch = document.querySelector('#location-search');
+        const value = $event.target.value;
+        if(value.length > 3){
+          locationSearch.setAttribute('list', 'datalist-locations');
+        }
+        else { 
+          locationSearch.setAttribute('list', null);
+         }
+      },
+      populateLocationsList(){
+        let dataList = document.querySelector('#datalist-locations');
+        cities.forEach(item => {
+          const option = document.createElement('option');
+          option.value = `${item.city}, ${item.state}`;
+          dataList.appendChild(option);
+        })
+      },
+      clearSearch(){
+        this.$store.commit(SET_JOB_SEARCH, { isSearching: false });
+      },
+      doSearch(){
+        this.$store.commit(SET_JOB_SEARCH, {
+          isSearching: true,
+          searchCriteria: this.searchCriteria,
+          searchLocation: this.searchLocation
+        });
+      },
+    },
+    mounted() {
+      this.populateLocationsList();
+    },
+    computed: {
+      searching(){
+        return this.$store.state.JobSearch.isSearching
+      }
+    },
   }
 </script>
 
@@ -50,10 +113,9 @@
   }
   .col {
     flex: 1;
-    width: 50%;
   }
   .search-inputs-container {
-    width: 600px;
+    width: 900px;
     background: #fff;
     border: 1px solid #ccc;
     border-radius: 5px;
@@ -91,7 +153,7 @@
   }
 
   .search-input {
-    width: 100%;
+    width: calc(100% - 10px);
     height: 36px;
     font-size: 1.3em;
     padding: 0;
@@ -112,6 +174,25 @@
     border: 0;
     position: relative;
     display: inline-block;
+  }
+
+  .search-button {
+    height: 56px;
+    border: 0;
+    background: #04cc0a;
+    color: #fff;
+    font-size: 24px;
+    font-weight: bold;
+    display:flex;
+    border-top-right-radius: 5px;
+    border-bottom-right-radius: 5px;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+
+    &:hover {
+      background:#2efa35;
+    }
   }
 
   @media screen and (max-width:576px) {
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,16 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import Nav from './nav';
+import Config from './config';
+import JobSearch from './job-search';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
-    Nav
+    Nav,
+    Config,
+    JobSearch,
   }
 });
 
